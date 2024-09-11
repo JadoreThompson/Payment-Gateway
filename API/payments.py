f804248874f6e34692b8d7c49d9f3f5a8997f874
LucioJamieAI <47329590+LucioJamieAI@users.noreply.github.com>
2024-09-11 21:12:22 +0100

    card details

--- API/payments.py
+++ API/payments.py
@@ -1,15 +1,17 @@
+from typing import Tuple
 import aiohttp
+from cryptography.fernet import Fernet
+import base64
 
 import os
 from dotenv import load_dotenv
-
-from urllib.parse import urlencode
 
 # Directory Modules
 from API.models import *
 
 # FastAPI Modules
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 
 
 load_dotenv("../.env")
@@ -20,6 +22,19 @@ HEADER = {
     "Authorization": f"Bearer {os.getenv("STRIPE_SANDBOX_SECRET_KEY")}"
 }
 
+
+FERNET_KEY = base64.b64encode(bytes(os.getenv("FERNET_KEY").encode()))
+fernet = Fernet(FERNET_KEY)
+
+
+def encrypt_data(data: str) -> str:
+    return fernet.encrypt(data.encode()).decode()
+
+
+def decrypt_data(encrypted_data: str) -> str:
+    return fernet.decrypt(encrypted_data.encode()).decode()
+
+
 # API Instantiation
 payments = APIRouter()
 
@@ -29,16 +44,27 @@ async def read_root():
     return {"status": 200, "message": "payment"}
 
 
-@payments.post("/payment_intent")
-async def create_payment(info: PaymentIntent):
-    async with aiohttp.ClientSession(headers=HEADER) as session:
+# Call this when user decides to enter basket as they have made their intent to pay
+@payments.post("/payment-intent")
+async def create_payment_intent(info: PaymentIntent):
+    """
+    :param info: class PaymentIntent
+    :return: str(Client Secret Key)
+    :notes: Allows client to securely create payment transaction
+    """
 
-        # First we get the currency and amount as the user's intent to purchase an item
-        async with session.post(
-                BASE_URL + "payment_intents",
-                data={"amount": info.amount, "currency": info.currency}
-        ) as rsp:
-            data = await rsp.json()
-            return data["client_secret"]
+    try:
+        async with aiohttp.ClientSession(headers=HEADER) as session:
+            async with session.post(
+                    BASE_URL + "payment_intents",
+                    data={"amount": info.amount, "currency": info.currency, "receipt_email": info.recipient_email}) as rsp:
+                data = await rsp.json()
+                encrypted_client_secret = encrypt_data(data["client_secret"])
+                return JSONResponse(status_code=200, content={"detail": encrypted_client_secret})
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"detail": f"{type(e)}, {str(e)}"})
 
 
+@payments.post("/create-payment-method")
+async def create_payment_method(payment_info: PaymentMethod):
+    return payment_info
