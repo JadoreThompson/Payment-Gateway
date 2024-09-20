513c0edd6d0ccc799a6178a2341eac2519bca45c
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2024-09-20 02:25:07 +0100

    wip login, signup pages

--- db_connection.py
+++ db_connection.py
@@ -18,21 +18,6 @@ conn_params = {
 conn_pool = psycopg2.pool.SimpleConnectionPool(minconn=1, maxconn=50, **conn_params)
 
 
-with psycopg2.connect(**conn_params) as conn:
-    with conn.cursor() as cur:
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS users(
-                id SERIAL PRIMARY KEY,
-                email VARCHAR(20) UNIQUE NOT NULL,
-                fname VARCHAR(20) NOT NULL,
-                sname VARCHAR(20) NOT NULL,
-                company_name VARCHAR(20),
-                password VARCHAR(100) NOT NULL
-            );            
-        """)
-        conn.commit()
-
-
 @contextmanager
 def get_db_conn():
     """
